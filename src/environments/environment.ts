e9da2a8f523c2306ab78e5c5b153a273ebaf98ed
BrunoDiasLS <brunodlopes82@globo.com>
2022-11-18 19:44:28 -0300

    Firebase

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+   firebaseConfig: {
+    apiKey: "AIzaSyAW5jZ8Lj5PI6l-ht4WdT3ukl4bPwpljyA",
+    authDomain: "app-colaboradores-a19a3.firebaseapp.com",
+    projectId: "app-colaboradores-a19a3",
+    storageBucket: "app-colaboradores-a19a3.appspot.com",
+    messagingSenderId: "612000221137",
+    appId: "1:612000221137:web:5b89b8a3d693ffd01a42f0"
+  }
 };
 
 /*
