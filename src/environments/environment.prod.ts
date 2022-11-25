76e8ec124661046d05b2415b42daf575f1b43ec2
BrunoDiasLS <brunodlopes82@globo.com>
2022-11-24 22:04:10 -0300

    finalização app

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+   apiKey: "AIzaSyAW5jZ8Lj5PI6l-ht4WdT3ukl4bPwpljyA",
+   authDomain: "app-colaboradores-a19a3.firebaseapp.com",
+   projectId: "app-colaboradores-a19a3",
+   storageBucket: "app-colaboradores-a19a3.appspot.com",
+   messagingSenderId: "612000221137",
+   appId: "1:612000221137:web:5b89b8a3d693ffd01a42f0"
+ }
 };
